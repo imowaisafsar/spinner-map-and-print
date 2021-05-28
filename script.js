2d6bef505393b9d064b77750a81247193db5ddad
owais-afsar <owais.afsar@outlook.com>
2021-05-28 13:28:25 +0500

    complete
    Everything set according to client requirements

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $("ul.number_list > li").click(function () {
+    if ($(this).hasClass('number_list-text')) {
+        return
+    }
     let rowId = $(this).attr('data-id')
     if (!$(this).hasClass('active_point')) {
         $(`ul.number_list-${rowId}`).each(function (a) {
@@ -45,89 +48,129 @@ const drawBetweenObjects = {
 }
 
 $("button#connect").click(function () {
+    $('#svg').css({
+        'width': $('body').width(),
+        'height': $('body').height()
+    })
     config.target = $(".active_point");
-    console.log(config.target)
     drawBetweenObjects.findLines(config.target);
     $(this).hide()
     $('#printPDF').show()
 })
+let Canvas;
 $("button#printPDF").click(function () {
-    $('div#mailModal').fadeIn('fast')
-    //printFn();
-    //test();
-    //test2()
+    if ($('body').height() > $(window).height()) {
+        $('#svg').css('left', '3px')
+    }
+    $('#printPDF').css('visibility', 'hidden')
+    var selector = document.getElementById('body')
+    html2canvas(selector).then(
+        function (canvas) {
+            Canvas = canvas;
+            console.log(canvas.toDataURL('image/jpeg', 0.9))
+        }).then(function () {
+            $('div#mailModal').fadeIn('fast')
+            $('#svg').css('left', '0px')
+        })
 })
-// async function test2() {
-//     let mCanvas;
-//     let div = document.getElementById('container');
-//     $('#output').empty();
-//     await html2canvas(div).then(
-//         function (canvas) {
-//             document.getElementById('output').appendChild(canvas);
-//             mCanvas = canvas;
-//         }).then(function () {
 
-//             var img = mCanvas.toDataURL("image/png");
-//             var doc = new jsPDF('l', 'mm', [297, 210]);
-//             doc.addImage(img, 'JPEG', 0, 0);
-//             // Making Data URI
-//             var out = doc.output();
-//             var url = 'data:application/pdf;base64,' + btoa(out);
+// Listen for FORM submit
+$(".contact-form").submit(function (e) {
+    submitForm(e)
+})
+function submitForm(e) {
+    e.preventDefault();
 
-//             Email.send({
-//                 Host: "smtp.gmail.com",
-//                 Username: "owaisafsar.mail@gmail.com",
-//                 Password: "nxpphjtlwnxfofzk",
-//                 To: "owaisafsar.mail@gmail.com",
-//                 From: "owaisafsar.mail@gmail.com",
-//                 Subject: `New Mail from "test"`,
-//                 Body: "owaisafsar.mail@gmail.com",
-//                 Attachments: [
-//                     {
-//                         name: "wheel-of-life-cycle.pdf",
-//                         data: url
-//                     }
-//                 ]
-//             })
-//                 .then((message) => {
-//                     console.log(message)
-//                 });
-//         });
-// }
+    $('.email_btn_loader').show()
+    let button = document.querySelector(".contact-submit");
+    button.setAttribute("disabled", "disabled");
 
-// var pdfBase64 = undefined;
-// function test() {
-//     let $pdf = document.getElementById("html")
-//     console.log($pdf)
-//     var opt = {
-//         margin: 0,
-//         filename: 'wheel-of-life-cycle.pdf',
-//         image: { type: 'jpeg', quality: 1 },
-//         html2canvas: { scale: 1 },
-//         jsPDF: { unit: 'in', format: 'letter', orientation: 'landscape' }
-//     };
+    //   Get input Values
+    let name = $("input.contact-name").val();
+    let email = $("input.contact-email").val();
+    if (name == "" || email == "") {
+        $(".modal-content").notify(
+            "Please fill details correctly.", "error",
+            { position: "bottom" }
+        )
+        button.removeAttribute("disabled");
+        $('.email_btn_loader').hide()
+        return
+    }
+    if (ValidateEmail(email) == "false") {
+        $(".modal-content").notify(
+            "Enter correct email address.", "error",
+            { position: "bottom" }
+        )
+        button.removeAttribute("disabled");
+        $('.email_btn_loader').hide()
+        return
+    }
+    //debugger;
+    sendEmail(name, email, button)
+}
+const ValidateEmail = (email) => {
+    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (regex.test(String(email).toLowerCase())) {
+        return "true";
+    }
+    else {
+        return "false";
+    }
+}
+function sendEmail(name, email, button) {
 
-//     // New Promise-based usage:
-//     // html2pdf().from($pdf).set(opt).save();
-//     pdfBase64 = html2pdf().from($pdf).set(opt);
-//     console.log(pdfBase64)
-// }
+    var img = Canvas.toDataURL("image/png", 0.8);
+    var doc = new jsPDF({
+        orientation: 'l', // landscape
+        unit: 'pt', // points, pixels won't work properly
+        format: [Canvas.width, Canvas.height] // set needed dimensions for any element
+    });
+    doc.addImage(img, 'JPEG', 0, 0, Canvas.width, Canvas.height);
+    // Making Data URI
+    var out = doc.output();
+    var pdfBase64 = 'data:application/pdf;base64,' + btoa(out);
 
-// async function printFn() {
-//     let div = document.getElementById('container');
-//     $('#output').empty();
-//     await html2canvas(div).then(
-//         function (canvas) {
-//             document
-//                 .getElementById('output')
-//                 .appendChild(canvas);
-//         });
-//     await printJS({
-//         printable: 'output',
-//         type: 'html',
-//         header: ''
-//     })
-// }
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "owaisafsar.mail@gmail.com",
+        Password: "nxpphjtlwnxfofzk",
+        To: email,
+        //Bcc: "info@wertelounge.de",
+        From: "owaisafsar.mail@gmail.com",
+        Subject: `New Mail from '${name}'`,
+        Body: `
+            <b>Sender Name:</b> ${name} <br>
+            <b>Sender Email:</b> ${email}`,
+        Attachments: [
+            {
+                name: "wheel-of-life-cycle.pdf",
+                data: pdfBase64
+            }
+        ]
+    })
+        .then((message) => {
+            console.log(message)
+            if (message === "OK") {
+                // reset form data
+                document.querySelector(".contact-form").reset();
+                // success message
+                $(".modal-content").notify(
+                    "Message has been send successfully! I will get back to you ASAP!", "success",
+                    { position: "bottom" }
+                );
+                $('div#mailModal').fadeOut('fast')
+            } else {
+                // error message
+                $(".modal-content").notify(
+                    "Something went wrong! Kindly pick another way to connect with me.", "error",
+                    { position: "bottom" }
+                );
+            }
+            button.removeAttribute("disabled");
+            $('.email_btn_loader').hide()
+        });
+}
 
 $(document).keypress("u", function (e) {
     if (e.ctrlKey) {
@@ -166,101 +209,4 @@ document.onkeydown = function (event) {
     } else {
         return true;
     }
-}
-
-// Listen for a submit
-document.querySelector(".contact-form").addEventListener("submit", submitForm);
-function submitForm(e) {
-    e.preventDefault();
-    let button = document.querySelector(".contact-submit");
-    button.setAttribute("disabled", "disabled");
-    $('.email_btn_loader').show()
-    //   Get input Values
-    let name = document.querySelector(".contact-name").value;
-    let email = document.querySelector(".contact-email").value;
-    if (name == "" || email == "") {
-        $(".modal-content").notify(
-            "Please fill details correctly.", "error",
-            { position: "bottom" }
-        );
-        button.removeAttribute("disabled");
-        $('.email_btn_loader').hide()
-        return
-    }
-    if (ValidateEmail(email) == "false") {
-        $(".modal-content").notify(
-            "Enter correct email address.", "error",
-            { position: "bottom" }
-        );
-        button.removeAttribute("disabled");
-        $('.email_btn_loader').hide()
-        return
-    }
-    sendEmail(name, email, button)
-}
-const ValidateEmail = (email) => {
-    const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (regex.test(String(email).toLowerCase())) {
-        return "true";
-    }
-    else {
-        return "false";
-    }
-}
-function sendEmail(name, email, button) {
-
-    let mCanvas;
-    let div = document.getElementById('container');
-    $('#output').empty();
-    html2canvas(div).then(
-        function (canvas) {
-            document.getElementById('output').appendChild(canvas);
-            mCanvas = canvas;
-        }).then(function () {
-
-            var img = mCanvas.toDataURL("image/png");
-            var doc = new jsPDF('l', 'mm', [297, 210]);
-            doc.addImage(img, 'JPEG', 0, 0);
-            // Making Data URI
-            var out = doc.output();
-            var url = 'data:application/pdf;base64,' + btoa(out);
-
-            Email.send({
-                Host: "smtp.gmail.com",
-                Username: "owaisafsar.mail@gmail.com",
-                Password: "nxpphjtlwnxfofzk",
-                // To: email,
-                // From: "owaisafsar.mail@gmail.com",
-                To: email,
-                From: "owaisafsar.mail@gmail.com",
-                Subject: `New Mail from '${name}'`,
-                Body: `Sender Name: <b>${name}</b> <br> Sender Email: <b>${email}</b>`,
-                Attachments: [
-                    {
-                        name: "wheel-of-life-cycle.pdf",
-                        data: url
-                    }
-                ]
-            })
-                .then((message) => {
-                    console.log(message)
-                    if (message === "OK") {
-                        document.querySelector(".contact-form").reset();
-                        // success message
-                        $(".modal-content").notify(
-                            "Message has been send successfully! I will get back to you ASAP!", "success",
-                            { position: "bottom" }
-                        );
-                        $('div#mailModal').fadeOut('fast')
-                    } else {
-                        // error message
-                        $(".modal-content").notify(
-                            "Something went wrong! Kindly pick another way to connect with me.", "error",
-                            { position: "bottom" }
-                        );
-                    }
-                    button.removeAttribute("disabled");
-                    $('.email_btn_loader').hide()
-                });
-        });
 }
